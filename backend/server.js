1797808d255a7c0217fabc3f98e65621c1929ef5
TheodoreBFrazier <theo.frazier@icloud.com>
2024-02-27 14:25:49 -0500

    creating serverr

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+//Create dependencies
+
+const app = require("./app.js");
+
+//Configuration
+
+require("dotenv").config();
+const PORT = process.env.PORT
+
+//Listen
+
+app.listen(PORT, () =>{
+    console.log(`listening to port ${PORT}`);
+});
