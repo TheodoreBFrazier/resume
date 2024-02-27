0815fc9f6cfd9045d0f7e5dc24a747054332ce18
TheodoreBFrazier <theo.frazier@icloud.com>
2024-02-27 13:34:32 -0500

    building database for job descriptions

---
+++ backend/db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS data_dev;
+
+CREATE DATABASE data_dev;
+
+\c data_dev;
+
+CREATE TABLE job_one (
+    job_id SERIAL PRIMARY KEY,
+    company_name VARCHAR(255),
+    job_title VARCHAR(255),
+    start_year INTEGER,
+    end_year INTEGER,
+    company_overview TEXT,
+    bullet_point_one TEXT,
+    bullet_point_two TEXT,
+    bullet_point_three TEXT,
+    bullet_point_four TEXT,
+    skill_one TEXT,
+    skill_two TEXT,
+    skill_three TEXT,
+    skill_four TEXT,
+    skill_five TEXT,
+    skill_six TEXT,
+    skill_seven TEXT,
+    skill_eight TEXT
+);
